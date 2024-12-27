130cbb5e2f17c1c38c03b8b6b8be55a980df0a33
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-28 00:09:28 +0530

    bubble short code

---
+++ DSA/bubble short code.cpp
@@ -0,0 +1,21 @@
+#include <iostream >
+using namespace std;
+
+void bubble(int arr[],int s)
+{
+	int c=0;
+	for(int i=0;i<s;i++)
+	{
+		for(int j=0;j<s-1,j++){
+			
+		}
+	}
+	
+}
+int main(){
+	int arr[]={5,1,4,3,9};
+
+	int s=sizeof(arr)/sizeof(arr[0]);
+
+
+}
