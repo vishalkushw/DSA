5f9fe0d7a4f04574053fe47494da80a34690c084
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2025-05-31 12:05:41 +0530

    Add two numbe

---
+++ DSA/Add two number.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+// void add(int num1,int num2){
+// 	
+// 	cout<<num1+num2;
+// }
+//
+//int main(){
+//	int num1;
+//	int num2;
+//	cout<<"enter first number : ";
+//	cin>>num1;
+//	cout<<"enter second number : ";
+//	cin>>num2;
+//	
+//	add(num1,num2);
+//	
+//}
+
+
